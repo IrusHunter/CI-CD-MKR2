50d34869a8e84c230b8a04cf79d32f391dee73b7
IrusHunter <korinevskiy.roman@gmail.com>
2025-05-23 11:19:38 +0300

    added image_detail view

--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Category
+from .models import Category, Image
 
 def gallery_view(request):
     categories = Category.objects.prefetch_related('image_set').all()
     return render(request, 'gallery.html', {'categories': categories})
 
 
-def image_detail(request, image_id):
-    pass
+def image_detail(request, pk):
+    image = get_object_or_404(Image, id=pk)
+    return render(request, 'image_detail.html', {'image': image})
